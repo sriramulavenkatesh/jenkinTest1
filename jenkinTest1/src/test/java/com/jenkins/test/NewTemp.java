141de8fd7b9e011165852df61896c4aff28c1486
sriramulavenkatesh <venkat7779@hotmail.com>
2019-02-25 08:58:34 +0530

    commit 858

---
+++ jenkinTest1/src/test/java/com/jenkins/test/NewTemp.java
@@ -0,0 +1,17 @@
+package com.jenkins.test;
+
+import org.testng.annotations.Test;
+
+public class NewTemp {
+	
+	
+	@Test
+	public void temp() {
+		
+		System.out.println("hello world");
+		
+		
+	}
+	
+
+}
